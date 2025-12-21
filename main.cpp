d2b4e67e9d2aebe593e0afa02fa73408d80b6f2e
ANV3L <conic648374@gmail.com>
2025-12-22 01:44:47 +0300

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "View/ViewModel.hpp"
+
+int main(int argc, char* argv[]) {
+
+    GraphFileSystem app;
+    app.run(argc, argv);
+
+    return 0;
+}
+
+// Реализоа динамическую подгрузку функций из библиотек а также создать динамическую библиотеку для демонстрации работы программы
